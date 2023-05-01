b70f9edb6e8258de3d43496f9cc4acd36ade4a8c
Diego Vergara Casanova <davc93@gmail.com>
2023-05-01 11:23:32 -0400

    adding colors

---
+++ apps/home/README.md
@@ -0,0 +1,3 @@
+## Base design
+
+- https://www.elegantthemes.com/layouts/food-drink/produce-box-landing-page/live-demo
--- apps/home/tailwind.config.js
+++ apps/home/tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#21401C',
+        primaryDark:'#173313',
+        primaryLight:'#52844A',
+        secondary:'#818D92',
+        secondaryLight:'#a3b9c2',
+        secondaryDark:'#576469',
+        tertiary:'#E44F17',
+        tertiaryLight:'#d5a667',
+        tertiaryDark:'#ad3c13',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/forms'),
